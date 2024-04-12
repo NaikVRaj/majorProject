ff2b1423643b183867f2d357303cba57e3f5db72
Vraj Naik <Vraj.Naik@iiitb.ac.in>
2024-04-13 00:26:52 +0530

    "Admin Controller"

--- src/main/java/com/spe/workwize/controllers/AdminController.java
+++ src/main/java/com/spe/workwize/controllers/AdminController.java
@@ -2,7 +2,6 @@ package com.spe.workwize.controllers;
 
 import com.spe.workwize.customModel.UserModel;
 import com.spe.workwize.service.Admin.AdminService;
-import com.spe.workwize.service.Admin.AdminServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @CrossOrigin("*")
 @RestController
@@ -35,32 +33,6 @@ public class AdminController {
             return ResponseEntity.ok(managers);
         }catch(Exception e){
             logger.error("[AdminController] - [Error in Get All Manager]");
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @GetMapping("/getEmployeeData")
-    public ResponseEntity<?> getAllEmployee()
-    {
-        try {
-            List<UserModel> emps = adminService.getAllEmployee();
-            logger.info("[AdminController] - [Get All Employee]");
-            return ResponseEntity.ok(emps);
-        }catch (Exception e){
-            logger.error("[AdminController] - [Error in Get All Employee]");
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @GetMapping("/deleteUser")
-    public ResponseEntity<?> deleteUser(@RequestParam Map<String,String> param)
-    {
-        try {
-            Map<String,String> res = adminService.deleteUser(param);
-            logger.info("[AdminController] - [Delete User]");
-            return ResponseEntity.ok(res);
-        }catch (Exception e){
-            logger.error("[AdminController] - [Error in Delete User]");
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
