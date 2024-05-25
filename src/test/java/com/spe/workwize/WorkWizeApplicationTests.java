a26c2efa9a76504dc941e43f6c855dac514b75be
VrajNaik <vrajnaik5421@gmail.com>
2024-05-25 23:27:56 +0530

    test

--- src/test/java/com/spe/workwize/WorkWizeApplicationTests.java
+++ src/test/java/com/spe/workwize/WorkWizeApplicationTests.java
@@ -1,8 +1,8 @@
 package com.spe.workwize;
 
 import com.spe.workwize.bean.User;
-import com.spe.workwize.service.admin.AdminService;
-import com.spe.workwize.service.user.UserService;
+import com.spe.workwize.service.Admin.AdminService;
+import com.spe.workwize.service.User.UserService;
 import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +15,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
